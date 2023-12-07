09223ebfae91210f481e2c2d8492f16a4e0753cc
Prima <primamalaky@gmail.com>
2023-12-07 15:57:37 +0100

    Ajout de clé étrangère aux models.

--- src/app/models.py
+++ src/app/models.py
@@ -12,13 +12,16 @@ class Facture(models.Model):
     numero_ordre = models.CharField(max_length=100)
     date_facture = models.DateField()
     montant_total = models.IntegerField()
+    client = models.ForeignKey(Client, on_delete=models.PROTECT)
 
 class Rapport(models.Model):
     nombre_bouteilles = models.IntegerField(default=0)
     poids_bouteilles = models.IntegerField(default=0)
     total_par_categories = models.IntegerField(default=0)
+    facture = models.ForeignKey(Facture, on_delete=models.CASCADE)
 
 
 class BordereauLivraison(models.Model):
     date_livraison = models.DateField()
     numero_bl = models.CharField(max_length=100)
+    facture = models.ForeignKey(Facture, on_delete=models.CASCADE)
